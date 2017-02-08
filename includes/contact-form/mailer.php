76b8ae3358b3ff10957d9485d40f39a9555992a8
Caio Marcelo Bizerril de Oliveira <caiofreesurf@gmail.com>
2017-02-08 13:15:10 -0200

    formulario de contato
    contato apontado para o email do christian.

--- includes/contact-form/mailer.php
+++ includes/contact-form/mailer.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "caio@castcupom.com";
+        $recipient = "chris.aranha7@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
